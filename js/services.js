b676e305e44a8df8b13abb573af2c0582d0bdf4e
ansontsang1112 <tch@s.hypernite.com>
2022-06-17 20:36:35 +0800

    Update services.js

--- js/services.js
+++ js/services.js
@@ -6,21 +6,29 @@ function getStatus() {
         if (this.readyState == 4 && this.status == 200) {
             srvObj = JSON.parse(this.responseText);
 
-            var orgArray = ["hkfl-1", "shd-1", "shd-2"];
+            let arrayMain = [];
+
+            var orgArray = ["hkfl-1", "shd-1", "shd-2", "shd-3", "fcts-1", "fcts-2", "hcn-1", "hcn-2", "mcmango-1", "mcmango-2"];
             
             // HKFL
-            var array0 = [srvObj.hkfl.Status, srvObj.hkfl.inetSocket]
+            arrayMain[0] = [srvObj.hkfl.Status, srvObj.hkfl.inetSocket, srvObj.hkfl.Latency];
             
-            // SHD1
-            var array1 = [srvObj.shd1.Status, srvObj.shd1.inetSocket]
+            // SHD
+            arrayMain[1] = [srvObj.shd1.Status, srvObj.shd1.inetSocket, srvObj.shd1.Latency]
+            arrayMain[2] = [srvObj.shd2.Status, srvObj.shd2.inetSocket, srvObj.shd2.Latency]
+            arrayMain[3] = [srvObj.shd3.Status, srvObj.shd3.inetSocket, srvObj.shd3.Latency]
 
-            // SHD2
-            var array2 = [srvObj.shd2.Status, srvObj.shd2.inetSocket]
+            // FCTS
+            arrayMain[4] = [srvObj.fcts1.Status, srvObj.fcts1.inetSocket, srvObj.fcts1.Latency]
+            arrayMain[5] = [srvObj.fcts2.Status, srvObj.fcts2.inetSocket, srvObj.fcts2.Latency]
+            
+            // HCN
+            arrayMain[6] = [srvObj.hcn1.Status, srvObj.hcn1.inetSocket, srvObj.hcn1.Latency]
+            arrayMain[7] = [srvObj.hcn2.Status, srvObj.hcn2.inetSocket, srvObj.hcn2.Latency]
 
-            let arrayMain = [];
-            arrayMain.push(array0);
-            arrayMain.push(array1);
-            arrayMain.push(array2);
+            // MCMANGO
+            arrayMain[8] = [srvObj.mcmango1.Status, srvObj.mcmango1.inetSocket, srvObj.mcmango1.Latency]
+            arrayMain[9] = [srvObj.mcmango2.Status, srvObj.mcmango2.inetSocket, srvObj.mcmango2.Latency]
 
             for(i = 0; i < arrayMain.length; i++) {
                 let statusLabel = orgArray[i];
@@ -32,7 +40,7 @@ function getStatus() {
                     sList.remove("label-default");
                     sList.add("label-danger");
                 } else {
-                    document.getElementById(statusLabel).innerHTML = "Operational 運作正常";
+                    document.getElementById(statusLabel).innerHTML = "Operational 運作正常" + "（連線延遲: " + arrayMain[i][2] + " ms）";
                     sList.remove("label-default");
                     sList.add("label-success");
                 }
@@ -58,7 +66,7 @@ function getStatus() {
 }
 
 function checkOperational() {
-    var groups = [["hkfl-1"], ["shd-1", "shd-2"]];
+    var groups = [["hkfl-1"], ["shd-1", "shd-2", "shd-3"], ["fcts-1", "fcts-2"], ["hcn-1", "hcn-2"], ["mcmango-1", "mcmango-2"]];
     var currentdate = new Date(); 
 
     for(i = 0; i < groups.length; i++) {
